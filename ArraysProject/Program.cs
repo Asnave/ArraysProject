98fd980023a3ec2d1015d80e8d61f5a8390375dd
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-10-12 11:08:38 -0300

    Fire

--- ArraysProject/Program.cs
+++ ArraysProject/Program.cs
@@ -8,8 +8,56 @@ namespace ArraysProject
 {
     class Program
     {
+        static int weapon = 0;
+        static int pistolAmmo = 6;
+        static int shotGunAmmo = 2;
+        static int bfgAmmo = 20;
+
+        static int[] WeaponAmmo;
+       static string[] weaponName = new string[3];
+        static int[] ammoCapacity;
+
         static void Main(string[] args)
         {
+          
+
+            WeaponAmmo = new int[3];
+            WeaponAmmo[0] = 0;
+            WeaponAmmo[1] = 0;
+            WeaponAmmo[2] = 0;
+
+            
+            weaponName[0] = "pistol";
+            weaponName[1] = "ShotGun";
+            weaponName[2] = "BFG";
+
+            ammoCapacity[0] = 6;
+            ammoCapacity[1] = 2;
+            ammoCapacity[2] = 20;
+
+            Fire();
+           
+            
+            Console.WriteLine(weaponName[weapon] + "Ammo: " + WeaponAmmo[weapon]);
+            Console.ReadKey(true);
         }
-    }
+
+        static void Reload()
+        {
+            if ( WeaponAmmo )
+          
+        }
+
+        static void Fire()
+        {
+            if ( WeaponAmmo[weapon] < 0)
+            {
+                WeaponAmmo[weapon] = WeaponAmmo[weapon] = 0 ;
+            }
+
+            WeaponAmmo[weapon] = WeaponAmmo[weapon] - 1;
+           
+        }
+
+     }   
 }
